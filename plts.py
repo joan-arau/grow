cbf5390b3c54fcb9f11c54bba66e94d8e3cb38f8
joan <joan.arau@me.com>
2020-12-19 14:11:54 +0100

    //

--- plts.py
+++ plts.py
@@ -1,6 +1,21 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import get_files
 
 
 
+get_files.get_temp()
+df = pd.read_csv('/Users/joan/PycharmProjects/grow/temp/data.csv')[['hum','moist','temp','time']]
+df['time'] = pd.to_datetime(df['time'], format='%m%d%Y%H%M%S')
+print(df)
 
+fig, axs = plt.subplots(3)
+
+axs[2].plot(df['time'],df['hum'])
+axs[2].set_title('Humidity')
+axs[0].plot(df['time'],df['moist'])
+axs[0].set_title('Soil Moisture')
+axs[1].plot(df['time'],df['temp'])
+axs[1].set_title('Temperature')
+
+plt.show()
